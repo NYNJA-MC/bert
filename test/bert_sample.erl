f26251b73566049b189923783967f3188af3fc6a
Thomas Arts <thomas.arts@quviq.com>
2020-03-28 17:11:07 +0100

    read sample.hrl from test dir

--- test/bert_sample.erl
+++ test/bert_sample.erl
@@ -1,6 +1,5 @@
 -module(bert_sample).
--include_lib("bert/include/sample.hrl").
+-include("sample.hrl").
 -include_lib("bert/include/bert.hrl").
 -compile({parse_transform, bert_google}).
 -compile({parse_transform, bert_validator}).
-
