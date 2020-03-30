1ab79065789c43d1133d7885079ff51dd0903a06
Thomas Arts <thomas.arts@quviq.com>
2020-03-30 17:43:04 +0200

    Adapt javascript to client side used javascript

--- src/bert_javascript.erl
+++ src/bert_javascript.erl
@@ -20,7 +20,7 @@ parse_transform(Forms, Options) ->
             NForms = filter(Forms, HrlFiles, {false, []}),
             File = filename:join(Dest, "json-bert.js"),
             filelib:ensure_dir(File),
-    io:format("Generated JavaScript: ~p~n", [File]),
+            io:format("Generated JavaScript: ~p~n", [File]),
             file:write_file(File, directives(NForms)),
             Forms
     end.
@@ -45,7 +45,7 @@ form(_Form) ->  [].
 
 prelude()  ->
     "function clean(r)      { for(var k in r) if(!r[k]) delete r[k]; return r; }\n"
-    "function check_len(x)  { try { return (eval('len'+utf8_dec(x.v[0].v))() == x.v.length) ? true : false }\n"
+    "function check_len(x)  { try { return (eval('len'+utf8_arr(x.v[0].v))() == x.v.length) ? true : false }\n"
     "                         catch (e) { return false; } }\n\n"
     "function scalar(data)    {\n"
     "    var res = undefined;\n"
@@ -63,24 +63,24 @@ decode(_F) -> lists:concat(["function decode(x) {\n"
     "    } else if (x.t == 108) {\n"
     "        var r = []; x.v.forEach(function(y) { r.push(decode(y)) }); return r;\n"
     "    } else if (x.t == 109) {\n"
-    "        return utf8_dec(x.v);\n"
+    "        return utf8_arr(x.v);\n"
     "    } else if (x.t == 104 && check_len(x)) {\n"
-    "        return eval('dec'+x.v[0].v)(x);\n"
+    "        return eval('dec' + x.v[0].v)(x);\n"
     "    } else if (x.t == 104) {\n"
-    "        var r=[]; x.v.forEach(function(a){r.push(decode(a))});\n"
-    "\treturn Object.assign({tup:'$'}, r);\n"
+    "        var r = []; x.v.forEach(function(a){r.push(decode(a))});\n"
+    "\treturn Object.assign({tup: '$'}, r);\n"
     "    } else return x.v;\n}\n\n"]).
 
 encode(_F) -> lists:concat([
     "function encode(x) {\n"
     "    if (Array.isArray(x)) {\n"
-    "        var r = []; x.forEach(function(y) { r.push(encode(y)) }); return {t:108,v:r};\n"
+    "        var r = []; x.forEach(function(y) { r.push(encode(y)) }); return {t: 108,v: r};\n"
     "    } else if (typeof x == 'object') {\n"
     "        switch (x.tup) {\n"
-    "\tcase '$': delete x['tup']; var r=[];\n"
+    "\tcase '$': delete x['tup']; var r = [];\n"
     "    Object.keys(x).map(function(p){return x[p];}).forEach(function(a){r.push(encode(a))});\n"
     "\treturn {t:104, v:r};\n"
-    "\tdefault: return eval('enc'+x.tup)(x); }\n"
+    "\tdefault: return eval('enc' + x.tup)(x); }\n"
     "    } else return scalar(x);\n}\n\n"]).
 
 %case_fields(Forms,Prefix) ->
@@ -94,23 +94,23 @@ decoder(List,T) ->
    Fields = filter_fields(T),
    case Fields of [] -> []; _ ->
    iolist_to_binary(["function len",L,"() { return ",integer_to_list(1+length(Fields)),"; }\n"
-                     "function dec",L,"(d) {\n    var r={}; ",
+                     "function dec",L,"(d) {\n    var r = {}; ",
      "r.tup = '",L,"';\n    ", string:join([ dispatch_dec(Type,Name,I) ||
      {{Name,Type},I} <- lists:zip(Fields,lists:seq(1,length(Fields))) ],";\n    "),
-     ";\n    return clean(r); }\n\n"]) end.
+     ";\n    return clean(r);\n}\n\n"]) end.
 
 encoder(List,T) ->
    Class = lists:concat([List]),
    Fields = filter_fields(T),
    Names = element(1,lists:unzip(Fields)),
    StrNames = case length(Fields) < 12 of
-                   true  -> string:join(Names,",");
-                   false -> {L,R} = lists:split(10,Names),
-                            string:join(L,",") ++ ",\n\t" ++ string:join(R,",") end,
+                   true  -> string:join(Names,", ");
+                   false -> {L,R} = lists:split(10, Names),
+                            string:join(L,", ") ++ ",\n\t" ++ string:join(R,", ") end,
    case Fields of [] -> []; _ ->
    iolist_to_binary(["function enc",Class,"(d) {\n    var tup = atom('",Class,"');\n    ",
      string:join([ dispatch_enc(Type,Name) || {Name,Type} <- Fields ],";\n    "),
-     ";\n    return tuple(tup,",StrNames,"); }\n\n"]) end.
+     ";\n    return tuple(tup, ", StrNames,");\n}\n\n"]) end.
 
 filter_fields(T) ->
   Fields = lists:flatten(
@@ -138,7 +138,7 @@ pack({Name,_Args}) -> io_lib:format("encode(d.~s)",[Name]).
 
 unpack({_Name,{X,_}},I) when X == tuple orelse X == term -> lists:concat(["decode(d.v[",I,"])"]);
 unpack({Name,{union,[{type,_,nil,[]},{type,_,Type,Args}]}},I) -> unpack({Name,{Type,Args}},I);
-unpack({_Name,{X,[]}},I) when X == binary -> lists:concat(["utf8_dec(d.v[",I,"].v)"]);
+unpack({_Name,{X,[]}},I) when X == binary -> lists:concat(["utf8_arr(d.v[",I,"].v)"]);
 unpack({_Name,{X,[]}},I) when X == integer orelse X == atom orelse X == list -> lists:concat(["d.v[",I,"].v"]);
 unpack({_Name,_Args},I) -> lists:concat(["decode(d.v[",I,"])"]).
 
@@ -155,11 +155,11 @@ dispatch_enc(Type,Name) ->
 
 enc_list(Name,Type) ->
     lists:flatten([
-    "var ",Name," = []; if ('",Name,"' in d && d.",Name,")\n\t {"
-    " d.",Name,".forEach(function(x){\n\t",Name,".push(",Type,"(x))});\n\t ", Name,"={t:108,v:",Name,"}; } else"
-    " { ",Name," = nil() }"]).
+    "var ",Name," = []; if ('",Name,"' in d && d.",Name,") {\n\t"
+    "d.",Name,".forEach(function(x) {\n\t\t",Name,".push(",Type,"(x))\n\t});\n\t", Name," = {t: 108, v: ",Name,"};\n\t} else"
+    "{ ",Name," = nil() }"]).
 
 dec_list(Name,I) ->
     lists:flatten(["r.",Name," = [];\n\t (d && d.v[",I,"] && d.v[",I,"].v) ?\n\t",
-        " d.v[",I,"].v.forEach(function(x){r.",Name,".push(decode(x))}) :\n\t",
+        " d.v[",I,"].v.forEach(function(x) { r.",Name,".push(decode(x)) }) :\n\t",
         " r.",Name," = undefined"]).
